8d5d78374bc33e9812e5153848950e64d90ee6fa
Ayan Das <bvits@riseup.net>
2025-04-11 14:10:37 +0530

    ebm: Setup project.

---
+++ manifest.scm
@@ -0,0 +1,10 @@
+(specifications->manifest '("python" "python-pytorch-cuda"
+                            "python-torchvision-cuda"
+                            "python-numpy"
+                            "python-matplotlib"
+                            "python-imageio"
+                            "python-pytest"
+                            "python-hypothesis"
+                            "python-ruff"
+                            "python-mypy"
+                            "python-pydocstyle"))
